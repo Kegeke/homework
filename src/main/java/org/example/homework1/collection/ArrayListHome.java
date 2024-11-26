3ee7a71ef7518314ad04b7d32215d3120a75e8a5
egor.djun <xedshotg@gmail.com>
2024-11-26 20:20:32 +0700

    add all files

--- src/main/java/org/example/homework1/collection/ArrayListHome.java
+++ src/main/java/org/example/homework1/collection/ArrayListHome.java
@@ -1,4 +1,149 @@
 package org.example.homework1.collection;
 
-public class ArrayListHome {
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArrayListHome<E> implements ListCollection<E> {
+    private static final int CAPACITY = 10;
+
+    private int size = 0;
+    private Object[] elements;
+
+    public ArrayListHome() {
+        elements = new Object[CAPACITY];
+    }
+
+    public ArrayListHome(int capacity) {
+        if (capacity > 0) {
+            elements = new Object[capacity];
+        } else if (capacity == 0) {
+            elements = new Object[]{};
+        } else {
+            throw new IllegalArgumentException("Illegal capacity : " + capacity);
+        }
+    }
+
+    @Override
+    public void add(E e) {
+        if (size == elements.length) {
+            grow(size + 1);
+        }
+        elements[size] = e;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E e) {
+        checkIndex(index);
+
+        if (size == elements.length) {
+            grow(size + 1);
+        }
+
+        Object[] elem = new Object[elements.length];
+
+        for (int i = 0; i < index; i++) {
+            elem[i] = elements[i];
+        }
+
+        elem[index] = e;
+
+        for (int i = index + 1; i < elements.length; i++) {
+            elem[i] = elements[i - 1];
+        }
+
+        size++;
+        elements = elem;
+    }
+
+    @Override
+    public E get(int index) {
+        checkIndex(index);
+
+        return (E) elements[index];
+    }
+
+    @Override
+    public E remove(int index) {
+        checkIndex(index);
+
+        E oldElem = (E) elements[index];
+
+        Object[] elem = new Object[size];
+
+        for (int i = 0; i < index; i++) {
+            elem[i] = elements[i];
+        }
+
+        for (int i = index ; i < elements.length; i++) {
+            if (i + 1 > size) {
+                break;
+            }
+            elem[i] = elements[i + 1];
+        }
+
+        elements = elem;
+        size--;
+
+        return oldElem;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < elements.length; i++) {
+            elements[i] = null;
+        }
+    }
+
+    @Override
+    public void sort(Comparator<? super E> comparator) {
+        /*
+        `````````````````````````````````
+         */
+    }
+
+    @Override
+    public E set(E e, int index) {
+        checkIndex(index);
+
+        E oldElement = (E) elements[index];
+        elements[index] = e;
+        return oldElement;
+    }
+
+    private void grow(int size) {
+        int newCapacity = (size * 3) / 2 + 1;
+
+        Object[] elem = new Object[newCapacity];
+        copy(elem);
+
+        elements = elem;
+    }
+
+    private void copy(Object[] objects) {
+        for (int i = 0; i < size; i++) {
+            objects[i] = elements[i];
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < elements.length; i++) {
+            System.out.println(elements[i]);
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index < 0 || index >= elements.length) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void trimToSize(){
+        Object [] elem = new Object[size];
+        copy(elem);
+    }
 }
--- src/main/java/org/example/homework1/collection/ListCollection.java
+++ src/main/java/org/example/homework1/collection/ListCollection.java
@@ -1,2 +1,19 @@
-package org.example.homework1.collection;public class ListCollection {
+package org.example.homework1.collection;
+
+import java.util.Comparator;
+
+interface ListCollection<E> {
+    void add(E obj);
+
+    void add(int index, E obj);
+
+    E get(int index);
+
+    E remove(int index);
+
+    void clear();
+
+    void sort(Comparator<? super E> comparator);
+
+    E set(E obj, int index);
 }
