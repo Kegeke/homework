e8906ee6351ca44faa0553ba6d2df441949b8006
egor.djun <xedshotg@gmail.com>
2024-11-26 10:39:52 +0700

    start project

---
+++ src/main/java/org/example/homework1/collection/ArrayListHome.java
@@ -0,0 +1,4 @@
+package org.example.homework1.collection;
+
+public class ArrayListHome {
+}
---
+++ src/test/java/org/example/homework1/collection/ArrayListHomeTest.java
@@ -0,0 +1,7 @@
+package org.example.homework1.collection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArrayListHomeTest {
+
+}
